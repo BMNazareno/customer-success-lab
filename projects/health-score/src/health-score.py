f9d56bc5c5aac394c911028195ec6bbefe68f9cc
BMNazareno <nazarenoveneno@outlook.com>
2026-07-22 15:26:06 -0300

    Add script

--- projects/health-score/src/health-score.py
+++ projects/health-score/src/health-score.py
@@ -1,6 +1,15 @@
+from pathlib import Path
+import pandas as pd
+
+
 def main():
-    print("Health Score project initialized.")
+    base_path = Path(__file__).parent.parent
+    csv_path = base_path / "data" / "clientes.csv"
+
+    df = pd.read_csv(csv_path)
+
+    print(df)
 
 
 if __name__ == "__main__":
-    main()
+    main() 
